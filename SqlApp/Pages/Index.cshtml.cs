b4707a6d268ba4fae22b516d01da3fe01e716d93
Karunakar B <karunakar1008@gmail.com>
2026-04-28 10:10:09 +0530

    Update Index.cshtml.cs

--- SqlApp/Pages/Index.cshtml.cs
+++ SqlApp/Pages/Index.cshtml.cs
@@ -5,16 +5,16 @@ namespace SqlApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IProductService _productService;
+        public List<Product> Products;
+    
+        public IndexModel(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
-
         public void OnGet()
         {
-
+            Products = _productService.GetProducts();
         }
     }
 }
