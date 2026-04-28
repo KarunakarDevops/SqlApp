5a0851a32a71d9f89aab4cc74180eaace2cae780
Karunakar B <karunakar1008@gmail.com>
2026-04-28 10:08:09 +0530

    Update ProductService.cs

--- SqlApp/ProductService.cs
+++ SqlApp/ProductService.cs
@@ -1,1 +1,58 @@
 
+using System.Data.SqlClient;
+
+namespace SqlApp
+{
+    public class ProductService : IProductService
+    {
+        private readonly IConfiguration _configuration;
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private SqlConnection GetConnection()
+        {
+
+            return new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
+        }
+        public List<Product> GetProducts()
+        {
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            SqlConnection _connection = GetConnection();
+
+            _connection.Open();
+
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();
+            return _product_lst;
+        }
+
+    }
+
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
